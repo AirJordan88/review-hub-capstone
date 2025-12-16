fdc6e1fc9daec5eaa58544e5be3d90cbd7936334
unknown <sturosair@gmail.com>
2025-12-15 19:25:52 -0500

    created debug logs

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import app from "#app";
 import db from "#db/client.js";
+import { URL } from "node:url";
 
 const PORT = process.env.PORT ?? 3000;
 
 async function start() {
   try {
+    console.log("NODE_ENV =", process.env.NODE_ENV);
+    console.log("DATABASE_URL is set =", Boolean(process.env.DATABASE_URL));
+    if (process.env.DATABASE_URL) {
+      console.log(
+        "DATABASE_URL host =",
+        new URL(process.env.DATABASE_URL).host
+      );
+    }
     await db.connect();
     console.log("Database connected.");
 
